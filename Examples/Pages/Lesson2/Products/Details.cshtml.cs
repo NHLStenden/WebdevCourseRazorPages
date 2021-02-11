9916826435b7f36400d73c679803b38465df247d
Joris Lops <jorislops@gmail.com>
2021-02-11 14:06:58 +0100

    nieuwe voorbeelden en aanpassingen

---
+++ Examples/Pages/Lesson2/Products/Details.cshtml.cs
@@ -0,0 +1,22 @@
+using Examples.Pages.Lesson2.Models;
+using Examples.Pages.Lesson2.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Examples.Pages.Lesson2.Products
+{
+    public class Details : PageModel
+    {
+
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+
+        public void OnGet([FromRoute] int productId)
+        {
+            var productsRepository = new ProductsRepository();
+            Product = productsRepository.GetProductById(productId);
+        }
+    }
+}

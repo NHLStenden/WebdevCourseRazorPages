cfb16be8ea456d5346003419cd20d1ec14c14ea7
Joris Lops <jorislops@gmail.com>
2021-03-01 08:24:33 +0100

    Removed LiveSharp, kan problemen veroorzaken.  (Kan je eventueel zelf toevoegen https://www.livesharp.net/ mocht je hot reload willen)

--- Examples/Pages/Lesson3/LesDemo/Todo.cs
+++ Examples/Pages/Lesson3/LesDemo/Todo.cs
@@ -2,20 +2,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Examples.Pages.Lesson3.LesDemo
 {
+    // CREATE DATABASE IF NOT EXISTS TodoDBExample;
+    // USE TodoDBExample;
     // CREATE TABLE Todo (
     //      TodoId INT PRIMARY KEY AUTO_INCREMENT,
     //      Description VARCHAR(20) NOT NULL,
     //      Done BOOLEAN DEFAULT FALSE
     // );
 
-    //INSERT INTO Todo (Description, Done) VALUES ('Item 1', FALSE);
-    //INSERT INTO Todo (Description, Done) VALUES ('Item 2', TRUE);
+    // INSERT INTO Todo (Description, Done) VALUES ('Item 1', FALSE);
+    // INSERT INTO Todo (Description, Done) VALUES ('Item 2', TRUE);
     //SELECT * FROM Todo;
 
     public class Todo
     {
         public int TodoId { get; set; }
-        [Required, MinLength(2), MaxLength(20)]
+        [Required(), MinLength(2), MaxLength(20)]
         public string Description { get; set; }
         public bool Done { get; set; }
     }
